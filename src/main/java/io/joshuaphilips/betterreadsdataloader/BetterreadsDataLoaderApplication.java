2d73db404634f1283d682fb66b2df31855ad888d
joshuaphilips <philipsjoshua96@gmail.com>
2021-11-09 16:27:16 +0000

    upload books to database

--- src/main/java/io/joshuaphilips/betterreadsdataloader/BetterreadsDataLoaderApplication.java
+++ src/main/java/io/joshuaphilips/betterreadsdataloader/BetterreadsDataLoaderApplication.java
@@ -4,10 +4,16 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import javax.annotation.PostConstruct;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +26,8 @@ import org.springframework.context.annotation.Bean;
 
 import io.joshuaphilips.betterreadsdataloader.author.Author;
 import io.joshuaphilips.betterreadsdataloader.author.AuthorRepository;
+import io.joshuaphilips.betterreadsdataloader.book.Book;
+import io.joshuaphilips.betterreadsdataloader.book.BookRepository;
 import io.joshuaphilips.betterreadsdataloader.connection.DataStaxAstraProperties;
 
 @SpringBootApplication
@@ -28,6 +36,9 @@ public class BetterreadsDataLoaderApplication {
 
 	@Autowired
 	AuthorRepository authorRepository;
+
+	@Autowired
+	BookRepository bookRepository;
 
 	@Value("${datadump.location.author}")
 	private String authorDumpLocation;
@@ -69,11 +80,77 @@ public class BetterreadsDataLoaderApplication {
 	}
 
 	private void initWorks() {
+		Path path = Paths.get(worksDumpLocation);
+		DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSS");
+		try (Stream<String> lines = Files.lines(path)) {
+			lines.forEach(line -> {
+				// read and parse the line
+				String jsonString = line.substring(line.indexOf("{"));
+				try {
+					JSONObject jsonObject = new JSONObject(jsonString);
+
+					// construct book object
+					Book book = new Book();
+					book.setId(jsonObject.getString("key").replace("/works/", ""));
+					book.setName(jsonObject.optString("title"));
+					JSONObject descriptionObj = jsonObject.optJSONObject("description");
+					if (descriptionObj != null) {
+						book.setDescription(descriptionObj.optString("value"));
+					}
+
+					JSONObject publishedObj = jsonObject.optJSONObject("created");
+					if (publishedObj != null) {
+						String dateStr = publishedObj.getString("value");
+						book.setPublishedDate(LocalDate.parse(dateStr, dateFormat));
+					}
+
+					JSONArray coversJsonArr = jsonObject.optJSONArray("covers");
+					if (coversJsonArr != null) {
+						List<String> coverIds = new ArrayList<>();
+						for (int i = 0; i < coversJsonArr.length(); i++) {
+							coverIds.add(coversJsonArr.getString(i));
+						}
+						book.setCoverIds(coverIds);
+
+					}
+
+					JSONArray authorsJsonArr = jsonObject.optJSONArray("authors");
+					if (authorsJsonArr != null) {
+						List<String> authorIds = new ArrayList<>();
+						for (int i = 0; i < authorsJsonArr.length(); i++) {
+							String authorId = authorsJsonArr.getJSONObject(i).getJSONObject("author").getString("key")
+									.replace("/authors/", "");
+							authorIds.add(authorId);
+						}
+
+						book.setAuthorIds(authorIds);
+
+						// get author names already in cassandra to set author name of book
+						List<String> authorNames = authorIds.stream().map(id -> authorRepository.findById(id))
+								.map(optionalAuthor -> {
+									if (!optionalAuthor.isPresent())
+										return "Unknown Author";
+									return optionalAuthor.get().getName();
+								}).collect(Collectors.toList());
+						book.setAuthorNames(authorNames);
+
+					}
+
+					System.out.println("Saving Book " + book.getName());
+					bookRepository.save(book);
+
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+			});
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@PostConstruct
 	public void start() {
-		initAuthors();
+		// initAuthors();
 		initWorks();
 	}
 
